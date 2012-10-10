aa19434f8be9e83d61ebfa668f58e27205cb2411
Myles Eftos <myles@madpilot.com.au>
2012-10-11 00:47:07 +0800

    Using 0.2.0 of the gem. Added slightly better error reporting. Show match example

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -2,8 +2,10 @@ class PlayersController < ApplicationController
   def index
     @players = Player.from_query(params[:query]) if params[:query]
     @players = Player.all unless params[:query]
-  rescue Parslet::ParseFailed
-    @players = Player.all 
-    flash.now[:error] = "Parsing query failed"
+  rescue Wherewolf::ParseError => e
+    @players = Player.all
+    @error = e
+    range = params[:query][e.position-4..e.position+4]
+    flash.now[:error] = "Parsing query failed near position: #{e.position}"
   end
 end
